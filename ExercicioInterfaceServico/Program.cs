74ce3d4a08d097110d8e74b47c80555787ff3e19
Sender Rodrigues dos Santos <sender.rsantos@gmail.com>
2020-03-05 11:08:30 -0300

    Add project files.

---
+++ ExercicioInterfaceServico/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ExercicioInterfaceServico {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
